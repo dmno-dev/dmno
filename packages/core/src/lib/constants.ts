0a9397b3f65308a899fde1cf4b42c3514ab73fb2
Theo Ephraim <theo@dmno.dev>
2024-05-09 10:34:37 -0700

    many improvements for dmno init (#65)

--- packages/core/src/lib/constants.ts
+++ packages/core/src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const GITHUB_REPO_URL = 'https://github.com/dmno-dev/core';
+export const GITHUB_REPO_URL = 'https://github.com/dmno-dev/dmno';
 export const DISCORD_INVITE_URL = 'https://chat.dmno.dev';
