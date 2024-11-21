425cd333e48aa4bb2e08b8b2fa1b5b70e0477213
Theo Ephraim <theo@dmno.dev>
2024-11-20 21:44:26 -0800

    fix NodeEnvType TS issue

--- packages/core/src/config-engine/data-types.ts
+++ packages/core/src/config-engine/data-types.ts
@@ -1,5 +1,9 @@
 import { createDmnoDataType, DmnoBaseTypes } from './configraph-adapter';
 
+//! IMPORTANT - without this import, TS gets confused about these being the same thing
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type { ConfigraphDataTypeFactoryFn } from '@dmno/configraph';
+
 // example of defining common type using our base types
 export const NodeEnvType = createDmnoDataType({
   // TODO: might want to split the base types from these? (both in "dmno/" for now)
